5c8fcd1fd807719f3cd7ca96b51a3e39ed1def5f
GauravHeera <gauravheera001@gmail.com>
2025-05-26 20:53:57 +0530

    Fix routing for GitHub Pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import { useState } from 'react'
+import { Routes, Route } from "react-router-dom"
 import Background from './components/Background'
 import Menu from './components/Menu'
 import Home from './components/Home'
@@ -7,22 +6,17 @@ import Templates from "./components/Templates"
 import Blogs from "./components/Blogs"
 import Contact from "./components/Contact"
 
-
 function App() {
-
   return (
     <>
       <Background/>
-      <Router>
-        <Menu/>
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/contact" element={<Contact/>}/>
-          <Route path="/templates" element={<Templates/>}/>
-          <Route path="/blogs" element={<Blogs/>}/>
-        </Routes>
-      </Router>
-      
+      <Menu/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/templates" element={<Templates/>}/>
+        <Route path="/blogs" element={<Blogs/>}/>
+      </Routes>
     </>
   )
 }
